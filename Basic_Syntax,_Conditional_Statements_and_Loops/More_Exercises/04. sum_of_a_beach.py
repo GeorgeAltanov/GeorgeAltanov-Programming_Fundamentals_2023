fcb0fad06f8f792fede69e5b101aa5c4d51dd37e
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-05-30 15:33:05 +0300

    Create 04. sum_of_a_beach.py

---
+++ Basic_Syntax,_Conditional_Statements_and_Loops/More_Exercises/04. sum_of_a_beach.py
@@ -0,0 +1,11 @@
+text = input()
+characters = text.lower()
+
+a = characters.count("sand")
+b = characters.count("water")
+c = characters.count("fish")
+d = characters.count("sun")
+
+points = a + b + c + d
+
+print(points)
