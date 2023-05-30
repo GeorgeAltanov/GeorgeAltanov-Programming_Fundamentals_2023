7a213a46b4e41c242631c5959a9045d89e3b1780
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-05-30 15:34:32 +0300

    Create 01. exchange_integers.py

---
+++ Data_Types_and_Variables/More_Exercises/01. exchange_integers.py
@@ -0,0 +1,11 @@
+first_number = int(input())
+second_number = int(input())
+
+third_number = second_number
+fourth_number = first_number
+
+print(f"Before:\n"
+      f"a = {fourth_number}\n"
+      f"b = {second_number}\nAfter:\n"
+      f"a = {third_number}\n"
+      f"b = {first_number}")
