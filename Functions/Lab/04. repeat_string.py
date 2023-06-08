b03c35a1a8b040b0b393ad392437906b7319992d
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-06-08 09:20:24 +0300

    Create 04. repeat_string.py

---
+++ Functions/Lab/04. repeat_string.py
@@ -0,0 +1,19 @@
+repeat_string = lambda string, n: string * n
+
+input_string = input()
+number_of_repeating = int(input())
+
+print(repeat_string(input_string, number_of_repeating))
+
+# input_string = input()
+# counter_n = int(input())
+#
+#
+# def new_string():
+#     result = ""
+#     for num in range(counter_n):
+#         result += input_string
+#     return result
+#
+#
+# print(new_string())
