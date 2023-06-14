706b441080b8a7d01098bbba3327e06ddf794401
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-06-14 09:31:46 +0300

    02. largest_of_three_numbers.py

--- Basic_Syntax,_Conditional_Statements_and_Loops/Lab/02. largest_of_three_numbers.py
+++ Basic_Syntax,_Conditional_Statements_and_Loops/Lab/02. largest_of_three_numbers.py
@@ -1,2 +1,20 @@
+# Write a program that receives three whole numbers and prints the largest one.
+#
+# Input1:
+# 3
+# -1
+# 5
+#
+# Output1: 5
+#
+# Input2:
+# 0
+# -1
+# -2
+#
+# Output2: 0
+
+
+
 num1, num2, num3 = int(input()), int(input()), int(input())
 print(max(num1, num2, num3))
