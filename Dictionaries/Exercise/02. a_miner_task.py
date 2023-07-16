7bf8b04c05bc9d79b770c0c051483a0ab191250a
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-07-16 13:11:27 +0300

    Create 02. a_miner_task.py

---
+++ Dictionaries/Exercise/02. a_miner_task.py
@@ -0,0 +1,15 @@
+resource = {}
+
+while True:
+    current_resource = input()
+    if current_resource == "stop":
+        break
+
+    current_quantity = int(input())
+
+    if current_resource not in resource.keys():
+        resource[current_resource] = 0
+    resource[current_resource] += current_quantity
+
+for product, quantity in resource.items():
+    print(f"{product} -> {quantity}")
