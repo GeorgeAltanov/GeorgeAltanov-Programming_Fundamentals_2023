7c3a37d17d7107803e78a5fbdf44e41451249d21
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-08-17 12:39:17 +0300

    Update 01. count_chars_in_a_string.py

--- Dictionaries/Exercise/01. count_chars_in_a_string.py
+++ Dictionaries/Exercise/01. count_chars_in_a_string.py
@@ -1,3 +1,20 @@
+# Write a program that counts all characters in a string except for space (" ").
+# Print all the occurrences in the following format:
+# "{char} -> {occurrences}"
+# Input1: text
+# Output1:
+# t -> 2
+# e -> 1
+# x -> 1
+#
+# Input2: text text text
+# Output2:
+# t -> 6
+# e -> 3
+# x -> 3
+
+
+
 symbols = [char for char in input() if char != " "]
 
 letters = {}
