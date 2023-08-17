0d972502acd6e724c880ba055eab5c1fe41767dd
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-08-17 12:40:20 +0300

    Update 04. phonebook.py

--- Dictionaries/Exercise/04. phonebook.py
+++ Dictionaries/Exercise/04. phonebook.py
@@ -1,3 +1,37 @@
+# Write a program that receives info from the console about people and their phone numbers.
+# Each entry should have a name and a number (both strings) separated by a "-".
+# If you receive a name that already exists in the phonebook, update its number.
+# After filling the phonebook, you will receive a number – N.
+# Your program should be able to perform a search of contact by name and print its details in the format: "{name} -> {number}".
+# In case the contact isn't found, print: "Contact {name} does not exist."
+# Input1:
+# Adam-0888080808
+# 2
+# Mery
+# Adam
+#
+# Output1:
+# Contact Mery does not exist.
+# Adam -> 0888080808
+#
+# Input2:
+# Adam-+359888001122
+# Ralf-666
+# George-5559393
+# Silvester-02/987665544
+# 4
+# Silvester
+# silvester
+# Rolf
+# Ralf
+#
+# Output2:
+# Silvester -> 02/987665544
+# Contact silvester does not exist.
+# Contact Rolf does not exist.
+# Ralf -> 666
+
+
 phonebook = {}
 
 while True:
