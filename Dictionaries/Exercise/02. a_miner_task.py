5874985f619708f270a125294db0849eb64502ae
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-08-17 12:39:39 +0300

    Update 02. a_miner_task.py

--- Dictionaries/Exercise/02. a_miner_task.py
+++ Dictionaries/Exercise/02. a_miner_task.py
@@ -1,3 +1,41 @@
+# You will be given a sequence of strings, each on a new line until you receive the "stop" command.
+# Every odd line on the console represents a resource (e.g., Gold, Silver, Copper, and so on) and every even - quantity.
+# Your task is to collect the resources and print them each on a new line.
+# Print the resources and their quantities in the following format:
+# "{resource} -> {quantity}"
+#
+# Input1:
+# Gold
+# 155
+# Silver
+# 10
+# Copper
+# 17
+# stop
+#
+# Output1:
+# Gold -> 155
+# Silver -> 10
+# Copper -> 17
+#
+# Input2:
+# gold
+# 155
+# silver
+# 10
+# copper
+# 17
+# gold
+# 15
+# stop
+#
+# Output2:
+# gold -> 170
+# silver -> 10
+# copper -> 17
+
+
+
 resource = {}
 
 while True:
