3bc6ee987015e29ee3c11723d9fd66576af27600
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-08-21 10:42:16 +0300

    Update 07. word_synonyms.py

--- Dictionaries/Lab/07. word_synonyms.py
+++ Dictionaries/Lab/07. word_synonyms.py
@@ -1,3 +1,38 @@
+# Write a program, which keeps a dictionary with synonyms. The key of the dictionary will be the word. The value will
+# be a list of all the synonyms of that word. You will be given a number n – the count of the words. After each term,
+# you will be given a synonym, so the count of lines you should read from the console is 2 * n. You will be receiving a
+# word and a synonym each on a separate line like this:
+# • {word}
+# • {synonym}
+# If you get the same word twice, just add the new synonym to the list.
+# Print the words in the following format:
+# {word} - {synonym1, synonym2 … synonymN}
+#
+# Input1:
+# 3
+# cute
+# adorable
+# cute
+# charming
+# smart
+# clever
+#
+# Output1:
+# cute - adorable, charming
+# smart - clever
+#
+# Input2:
+# 2
+# task
+# problem
+# task
+# assignment
+#
+# Output2:
+# task – problem, assignment
+
+
+
 count = int(input())
 synonyms = {}
 
