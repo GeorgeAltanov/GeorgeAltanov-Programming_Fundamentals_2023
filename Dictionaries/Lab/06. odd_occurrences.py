5d1ae2cfce5fdb0b2d1e51e21d1c41bdc33cbbc4
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-08-21 10:41:57 +0300

    Update 06. odd_occurrences.py

--- Dictionaries/Lab/06. odd_occurrences.py
+++ Dictionaries/Lab/06. odd_occurrences.py
@@ -1,3 +1,17 @@
+# Write a program that prints all elements from a given sequence of words that occur an odd number of times (case-insensitive) in it.
+# • Words are given on a single line, space-separated.
+# • Print the result elements in lowercase, in their order of appearance.
+#
+# Input1: Java C# PHP PHP JAVA C java
+# Output1: java c# c
+#
+# Input2: 3 5 5 hi pi HO Hi 5 ho 3 hi pi
+# Output2: 5 hi
+#
+# Input3: a a A SQL xx a xx a A a XX c
+# Output3: a sql xx c
+
+
 words = input().split()
 
 dictionary = {}
