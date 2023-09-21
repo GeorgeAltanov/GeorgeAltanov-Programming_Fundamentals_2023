346a944e9cab7f0aa87f6d2de7a63524126c7b05
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-09-21 22:52:06 +0300

    Update 05. orders.py

--- Functions/Lab/05. orders.py
+++ Functions/Lab/05. orders.py
@@ -1,3 +1,25 @@
+# Write a function that calculates the total price of an order and returns it.
+# The function should receive one of the following products: "coffee", "coke", "water", or "snacks", and a quantity of the product.
+# The prices for a single piece of each product are:
+# • coffee - 1.50
+# • water - 1.00
+# • coke - 1.40
+# • snacks - 2.00
+# Print the result formatted to the second decimal place.
+#
+# Input1:
+# water
+# 5
+#
+# Output1: 5.00
+#
+# Input2:
+# coffee
+# 2
+#
+# Output2: 3.00
+
+
 def order(product, returns):
     result = None
     if product == "coffee":
