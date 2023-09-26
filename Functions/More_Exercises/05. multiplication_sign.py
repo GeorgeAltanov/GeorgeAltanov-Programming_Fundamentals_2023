9253a640450fadf137b2deede481964ab3849632
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-09-26 21:30:52 +0300

    Update 05. multiplication_sign.py

--- Functions/More_Exercises/05. multiplication_sign.py
+++ Functions/More_Exercises/05. multiplication_sign.py
@@ -1,3 +1,23 @@
+# You will receive three integer numbers.
+# Write a program that finds if their multiplication (the result) is negative, positive, or zero.
+# Try to do this WITHOUT multiplying the 3 numbers.
+#
+# Input1:
+# 2
+# 3
+# -1
+#
+# Output1: negative
+#
+# Input2:
+# 2
+# 3
+# 1
+#
+# Output2: positive
+
+
+
 def multiplication(n_1, n_2, n_3):
     result = ""
     if n_1 * n_2 * n_3 < 0:
