f6b3a398977bd0bb3be4b944918cc26500e9bdb0
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-09-26 21:31:21 +0300

    Create 03.longer_line.py

---
+++ Functions/More_Exercises/03.longer_line.py
@@ -0,0 +1,66 @@
+# You will be given the coordinates of four points.
+# The first and the second pair of points form two different lines.
+# Create a function that prints the longer line in the format "({X1}, {Y1})({X2}, {Y2})"
+# starting from the point which is closer to the center of the coordinate system (0, 0).
+# You can reuse the method that you wrote for the previous problem. If the lines are of equal length, print only the first one.
+# The resulting coordinates must be formatted to the lower integer.
+#
+# Input1:
+# 2
+# 4
+# -1
+# 2
+# -5
+# -5
+# 4
+# -3
+#
+# Output1: (4, -3)(-5, -5)
+#
+# Input2:
+# 1
+# 2
+# 3
+# 4
+# 9
+# 7
+# 5
+# 6
+#
+# Output2: (5, 6)(9, 7)
+
+def calculate_distance_from_center(x1_coordinate_line1, y1_coordinate_line1, x2_coordinate_line1, y2_coordinate_line1,
+                                   x1_coordinate_line2, y1_coordinate_line2, x2_coordinate_line2, y2_coordinate_line2 ):
+    from math import floor
+    distance_from_center_point_1 = abs(x1_coordinate_line1) + abs(y1_coordinate_line1)
+    distance_from_center_point_2 = abs(x2_coordinate_line1) + abs(y2_coordinate_line1)
+    distance_from_center_point_3 = abs(x1_coordinate_line2) + abs(y1_coordinate_line2)
+    distance_from_center_point_4 = abs(x2_coordinate_line2) + abs(y2_coordinate_line2)
+    length_line_1 = abs(x1_coordinate_line1) + abs(y1_coordinate_line1) + abs(x2_coordinate_line1) + abs(y2_coordinate_line1)
+    length_line_2 = abs(x1_coordinate_line2) + abs(y1_coordinate_line2) + abs(x2_coordinate_line2) + abs(y2_coordinate_line2)
+
+    if length_line_1 >= length_line_2:
+        if distance_from_center_point_1 <= distance_from_center_point_2:
+            print(f'({floor(x1_coordinate_line1)}, {floor(y1_coordinate_line1)})({floor(x2_coordinate_line1)}, {floor(y2_coordinate_line1)})')
+        else:
+            print(f'({floor(x2_coordinate_line1)}, {floor(y2_coordinate_line1)})({floor(x1_coordinate_line1)}, {floor(y1_coordinate_line1)})')
+
+    else:
+        if distance_from_center_point_3 <= distance_from_center_point_4:
+            print(f'({floor(x1_coordinate_line2)}, {floor(y1_coordinate_line2)})({floor(x2_coordinate_line2)}, {floor(y2_coordinate_line2)})')
+        else:
+            print(f'({floor(x2_coordinate_line2)}, {floor(y2_coordinate_line2)})({floor(x1_coordinate_line2)}, {floor(y1_coordinate_line2)})')
+
+x_coordinate_point_1_line_1 = float(input())
+y_coordinate_point_1_line_1 = float(input())
+x_coordinate_point_2_line_1 = float(input())
+y_coordinate_point_2_line_1 = float(input())
+x_coordinate_point_1_line_2 = float(input())
+y_coordinate_point_1_line_2 = float(input())
+x_coordinate_point_2_line_2 = float(input())
+y_coordinate_point_2_line_2 = float(input())
+
+calculate_distance_from_center(x_coordinate_point_1_line_1, y_coordinate_point_1_line_1,
+                               x_coordinate_point_2_line_1, y_coordinate_point_2_line_1,
+                               x_coordinate_point_1_line_2, y_coordinate_point_1_line_2,
+                               x_coordinate_point_2_line_2, y_coordinate_point_2_line_2)
