d1a0ebb2482ee2afaf5c9311bf5ce9f3571fcfe6
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-09-27 20:52:11 +0300

    Update 03. word_filter.py

--- Lists_Advanced/Exercise/03. word_filter.py
+++ Lists_Advanced/Exercise/03. word_filter.py
@@ -1,3 +1,16 @@
+# Using comprehension, write a program that receives some text, separated by space, and take only those words whose length is even.
+# Print each word on a new line.
+#
+# Input1: kiwi orange banana apple
+# Output1:
+# kiwi
+# orange
+# banana
+#
+# Input2: pizza cake pasta chips
+# Output2: cake
+
+
 some_text = input().split()
 new_list = [word for word in some_text if len(word) % 2 == 0]
 
