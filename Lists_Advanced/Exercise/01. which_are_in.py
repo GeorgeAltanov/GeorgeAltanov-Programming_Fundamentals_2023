d9318f46c291869111f911ce28c0aaa140f53b40
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-09-27 20:51:23 +0300

    Update 01. which_are_in.py

--- Lists_Advanced/Exercise/01. which_are_in.py
+++ Lists_Advanced/Exercise/01. which_are_in.py
@@ -1,3 +1,19 @@
+# You will be given two sequences of strings, separated by ", ". Print a new list containing only the strings from the first input line,
+# which are substrings of any string in the second input line.
+#
+# Input1:
+# arp, live, strong
+# lively, alive, harp, sharp, armstrong
+#
+# Output1: ['arp', 'live', 'strong']
+#
+# Input2:
+# tarp, mice, bull
+# lively, alive, harp, sharp, armstrong
+#
+# Output2: []
+
+
 
 #1
 
