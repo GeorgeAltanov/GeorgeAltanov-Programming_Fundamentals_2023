d298ee1c56b9734b849d1fb1543bbdbbeea436ad
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-09-28 21:42:59 +0300

    Update 07. group_of_10's.py

--- Lists_Advanced/Exercise/07. group_of_10's.py
+++ Lists_Advanced/Exercise/07. group_of_10's.py
@@ -1,3 +1,27 @@
+# Write a program that receives a sequence of numbers (a string containing integers separated by ", ") and prints the numbers
+# sorted into lists of 10's in the format "Group of {group}'s: {list_of_numbers}".
+# Examples:
+# • The numbers 2, 8, 4, and 10 fall into the group of 10's.
+# • The numbers 13, 19, 14, and 15 fall into the group of 20's.
+# For more clarification, see the examples below.
+#
+# Input1: 8, 12, 38, 3, 17, 19, 25, 35, 50
+# Output1:
+# Group of 10's: [8, 3]
+# Group of 20's: [12, 17, 19]
+# Group of 30's: [25]
+# Group of 40's: [38, 35]
+# Group of 50's: [50]
+#
+# Input2: 1, 3, 3, 4, 34, 35, 25, 21, 33
+#
+# Output2:
+# Group of 10's: [1, 3, 3, 4]
+# Group of 20's: []
+# Group of 30's: [25, 21]
+# Group of 40's: [34, 35, 33]
+
+
 numbers = [int(s) for s in input().split(", ")]
 
 group = 10
