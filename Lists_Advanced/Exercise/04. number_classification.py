7d820b39277c2de9f64d28de9e910168c749a018
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-09-28 21:42:17 +0300

    Update 04. number_classification.py

--- Lists_Advanced/Exercise/04. number_classification.py
+++ Lists_Advanced/Exercise/04. number_classification.py
@@ -1,3 +1,22 @@
+# Using a list comprehension, write a program that receives numbers, separated by comma and space ", ", and prints
+# all the positive, negative, even, and odd numbers on separate lines as shown below.
+# Note: Zero is counted for a positive number
+#
+# Input1: 1, -2, 0, 5, 3, 4, -100, -20, 12, 19, -33
+# Output1:
+# Positive: 1, 0, 5, 3, 4, 12, 19
+# Negative: -2, -100, -20, -33
+# Even: -2, 0, 4, -100, -20, 12
+# Odd: 1, 5, 3, 19, -33
+#
+# Input2: 1, 2, 53, 2, 21
+# Output2:
+# Positive: 1, 2, 53, 2, 21
+# Negative:
+# Even: 2, 2
+# Odd: 1, 53, 21
+
+
 numbers = input().split(", ")
 
 positive_numbers = [number for number in numbers if int(number) >= 0]
