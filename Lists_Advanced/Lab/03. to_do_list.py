6fadeee544c998df4f0d5ed89189b1150f5db608
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-10-04 20:48:56 +0300

    Update 03. to_do_list.py

--- Lists_Advanced/Lab/03. to_do_list.py
+++ Lists_Advanced/Lab/03. to_do_list.py
@@ -1,3 +1,18 @@
+# You will be receiving to-do notes until you get the command "End".
+# The notes will be in the format: "{importance}-{note}".
+# Return a list containing all to-do notes sorted by importance in ascending order.
+# The importance value will always be an integer between 1 and 10 (inclusive).
+#
+# Input1:
+# 2-Walk the dog
+# 1-Drink coffee
+# 6-Dinner
+# 5-Work
+# End
+#
+# Output1:
+
+
 def process_todo_notes():
     todo_notes = []
 
