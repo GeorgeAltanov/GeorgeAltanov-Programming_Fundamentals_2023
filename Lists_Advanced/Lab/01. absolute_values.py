7ee3e53d460a51c43d70b8f0f06cada0704e7304
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-10-04 20:47:56 +0300

    Update 01. absolute_values.py

--- Lists_Advanced/Lab/01. absolute_values.py
+++ Lists_Advanced/Lab/01. absolute_values.py
@@ -1,3 +1,13 @@
+# Using comprehension, write a program that receives a text and removes all its vowels, case insensitive.
+# Print the new text string after removing the vowels. The vowels that should be considered are 'a', 'o', 'u', 'e', 'i'.
+#
+# Input1: Python
+# Output1: Pythn
+#
+# Input2: ILovePython
+# Output2: LvPythn
+
+
 text = input()
 
 vowels = ['a', 'o', 'u', 'e', 'i']
