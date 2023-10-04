1c15efe8e643fcbefa814079bd7a523554d049e5
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-10-04 20:48:21 +0300

    Update 02. trains.py

--- Lists_Advanced/Lab/02. trains.py
+++ Lists_Advanced/Lab/02. trains.py
@@ -1,3 +1,18 @@
+# You will receive a number representing the number of wagons a train has.
+# Create a list (train) with the given length containing only zeros.
+# Until you receive the command "End", you will receive some of the following commands:
+# • "add {people}" – you should add the number of people in the last wagon
+# • "insert {index} {people}" - you should add the number of people at the given wagon
+# • "leave {index} {people}" - you should remove the number of people from the wagon. There will be no case in which the people will be more than the count in the wagon.
+# There will be no case in which the index is invalid!
+# After you receive the "End" command print the train.
+#
+# Input1:
+# 3
+# add 20
+# insert 0 15
+
+
 number_of_wagons = int(input())
 
 train = number_of_wagons * [0]
