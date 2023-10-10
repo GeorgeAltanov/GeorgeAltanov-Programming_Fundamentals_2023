e4eba64d62a5ef23cb2673b7800501c1b3ebae4b
Georgi Altanov <126617687+GeorgeAltanov@users.noreply.github.com>
2023-10-10 21:09:46 +0300

    Update 05. sorting_names.py

--- Lists_Advanced/Lab/05. sorting_names.py
+++ Lists_Advanced/Lab/05. sorting_names.py
@@ -1,3 +1,16 @@
+# Write a program that reads a single string with names separated by comma and space ", ".
+# Sort the names by their length in descending order.
+# If 2 or more names have the same length, sort them in ascending order (alphabetically).
+# Print the resulting list.
+#
+# Input1: Ali, Marry, Kim, Teddy, Monika, John
+#
+# Output1: ["Monika", "Marry", "Teddy", "John", "Ali", "Kim"]
+#
+# Input2: Lilly, Tim, Kate, Tom, Alex
+#
+# Output2: ['Lilly', 'Alex', 'Kate', 'Tim', 'Tom']
+
 
 def sorted_list():
     names_list = input().split(", ")
